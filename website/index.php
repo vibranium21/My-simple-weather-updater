e0914409c4f60f95ffd15a129db1d14a65f734e2
vibranium21 <84234931+vibranium21@users.noreply.github.com>
2023-06-06 08:01:00 -0400

    Add user imput for which city

--- website/index.php
+++ website/index.php
@@ -7,11 +7,17 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h5>Weather Data:</h5>
-<?php
-    $api_key = "66f288d993da3c522897dd934449ab63"; // Replace with your OpenWeatherMap API key
-    $city = "Durham"; // Replace with the desired city
+    <form action="index.php" method="get"
+        <label>City:</label><br>
+        <input type="text" name="City:"><br>
+    </form>
+    
 
+    <h5>Weather Data:</h5>
+    
+<?php
+    $api_key = "66f288d993da3c522897dd934449ab63";
+    $city = "{$_GET["City:"]}";
     $url = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$api_key}";
     $data = file_get_contents($url);
     $weather_data = json_decode($data);
